fee826ce882023a8905db184d1a01d078307572b
vemireddyarun0701 <vemireddyarun0701@gmail.com>
2021-07-17 19:25:55 +0530

    fc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CareerComponent } from './career/career.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { QualificationComponent } from './qualification/qualification.component';
+import { SkillComponent } from './skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"career", component:CareerComponent},
+  {path:"qualification", component:QualificationComponent},
+  {path:"exp", component:ExperienceComponent},
+  {path:"skills", component:SkillComponent},
+  {path:"contact", component:ContactComponent},
+  {path: '',   redirectTo: '/career', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { QualificationComponent } from './qualification/qualification.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillComponent } from './skill/skill.component';
+import { CareerComponent } from './career/career.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QualificationComponent,
+    ExperienceComponent,
+    SkillComponent,
+    CareerComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
